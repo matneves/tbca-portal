0ebf796d8acafef7c12dacb201cd9f688fcf1aad
Mateus Neves <mateus@mateusneves.com.br>
2015-12-03 19:02:21 -0300

    analytics

--- includes/header.php
+++ includes/header.php
@@ -18,5 +18,15 @@
 	<script type="text/javascript" src="js/jquery.ui.js"></script>
 	<script type="text/javascript" src="js/bootstrap.js"></script>
 	<script type="text/javascript" src="js/portal.js"></script>
+
+	<script type="text/javascript">
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-70969881-1', 'auto');
+		ga('send', 'pageview');
+	</script>
 </head>
 <body>
